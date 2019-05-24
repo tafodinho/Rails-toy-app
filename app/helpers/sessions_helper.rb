cae66349da7003dd5196ce62b4b5d76e36708ab7
Tafang Joshua <tafodinho@gmail.com>
2019-05-24 12:09:16 +0100

    finished with the advance logout section

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,21 @@ module SessionsHelper
     end
 
     def current_user
-        if session[:user_id] 
+        if user_id = session[:user_id] 
             @current_user = @current_user || User.find_by_id(session[:user_id])
+        elsif user_id = cookies.signed[:user_id]
+            user = User.find_by_id(user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
+    end
+
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id 
+        cookies.permanent[:remember_token] = user.remember_token
     end
 
     def logged_in?
@@ -15,7 +27,16 @@ module SessionsHelper
     end
 
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
+
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255},
@@ -8,9 +12,28 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    def User.digest(string)
+    def self.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
+    
+    def self.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember 
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
 end
